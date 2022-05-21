e7ec01d656dca4faa17bf69f0736a5e643d8736b
KEENTO <handball0321@gmail.com>
2022-05-21 13:53:21 -0700

    Fetch data to CountryDetail.tsx

--- src/pages/CountryDetail.tsx
+++ src/pages/CountryDetail.tsx
@@ -1,23 +1,86 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, Fragment } from "react";
+import { CountryViewObj } from "../models/model";
+import axios from "axios";
+
+const initialState = {
+  name: "",
+  capital: "",
+  population: "",
+  continents: "",
+  currencies: "",
+  languages: "",
+  coatOfArms: "",
+  flagImg: "",
+  flagIcon: "",
+  cca3: "",
+  borders: "",
+};
 
 const CountryDetail: React.FC = () => {
-  const [countryCode, setCountryCode] = useState("");
+  const [countryData, setCountryData] = useState<any>(initialState);
+  const [isLoading, setIsLoading] = useState(false);
 
-  function getCountryCode() {
-    const pathName = window.location.pathname.toString();
-    const cca3Code = pathName.substring(pathName.length - 3, pathName.length);
-    setCountryCode(cca3Code);
+  function requestCountryData() {
+    setIsLoading(true);
+
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((res) => {
+        if (!res) throw new Error("Request failed.");
+        const resData = res.data;
+
+        const pathName = window.location.pathname.toString();
+        const currentCCA3 = pathName.substring(
+          pathName.length - 3,
+          pathName.length
+        );
+
+        console.log(currentCCA3);
+
+        for (const key in resData) {
+          if (resData[key].cca3 === currentCCA3) {
+            setCountryData({
+              name: resData[key].name.common,
+              capital: resData[key].capital,
+              population: resData[key].population,
+              continents: resData[key].continents,
+              currencies: resData[key].currencies,
+              languages: resData[key].languages,
+              coatOfArms: resData[key].coatOfArms.png,
+              flagImg: resData[key].flags.png,
+              flagIcon: resData[key].flag,
+              cca3: resData[key].cca3,
+              borders: resData[key].borders,
+            });
+            break;
+          }
+        }
+      })
+      .catch((error) => console.log(error.message));
+    setIsLoading(false);
   }
 
   useEffect(() => {
-    getCountryCode();
+    requestCountryData();
   }, []);
 
   return (
-    <div>
-      <p>Let's add the country detail page.</p>
-      <p>Now, cca3 code is {`${countryCode}`}</p>
-    </div>
+    <Fragment>
+      <div>
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && (
+          <div>
+            <p>
+              The info of {`${countryData.name ? countryData.name : ""}`} are
+              below.
+            </p>
+            <p>{`capital: ${countryData.capital}, population: ${countryData.population}, continents: ${countryData.continents}, currencies: ${countryData.currencies}, languages: ${countryData.languages}, flagIcon: ${countryData.flagIcon}, cca3: ${countryData.cca3}, borders: ${countryData.borders}`}</p>
+            <img src={`${countryData.coatOfArms}`} alt="coatOfArms" />
+            <img src={`${countryData.flagImg}`} alt="flag" />
+          </div>
+        )}
+      </div>
+    </Fragment>
   );
 };
 
