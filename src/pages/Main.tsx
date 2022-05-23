52296059141445254f25449fa8fd4a9c9ef6f8d4
KEENTO <handball0321@gmail.com>
2022-05-22 20:17:57 -0700

    Add BeenTo.tsx

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,12 +5,14 @@ import Home from "./Home";
 import Search from "./Search";
 import BucketList from "./BucketList";
 import NotFound from "./NotFound";
+import BeenTo from "./BeenTo";
 
 const Main = () => {
   return (
     <Routes>
       <Route path="/" element={<Hero />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/beento" element={<BeenTo />} />
       <Route path="/bucket-list" element={<BucketList />} />
       <Route path="/countries" element={<Search />} />
       <Route path="/countries/:id" element={<CountryDetail />} />
