7755df115ccbd4b43e491f992b0a7a4afe347801
KEENTO <handball0321@gmail.com>
2022-05-23 13:53:26 -0700

    Add appropriate bg in Region.tsx

---
+++ src/components/UI/RegionWrapper.tsx
@@ -0,0 +1,24 @@
+import Header from "../../layouts/Header";
+import { RegionWrapperType } from "../../models/model";
+import imgUrl from "../../assets/test-cardBg.jpg";
+
+const RegionWrapper = ({ children, imageUrl }: RegionWrapperType) => {
+  return (
+    <div
+      className="min-h-screen"
+      style={{
+        backgroundImage: `url(${imageUrl})`,
+        // backgroundSize: "cover",
+        backgroundPosition: "0% 0%",
+      }}
+    >
+      <div className="min-h-screen px-5 sm:px-8">
+        <Header />
+        <div className="pt-16">{children}</div>
+      </div>
+      ;
+    </div>
+  );
+};
+
+export default RegionWrapper;
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -24,6 +24,22 @@ const Search = () => {
   // declare navigate
   const navigate = useNavigate();
 
+  const regionArr: any = {
+    Asia: "https://images.unsplash.com/photo-1464817739973-0128fe77aaa1?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470",
+    Antarctica:
+      "https://images.unsplash.com/photo-1535752385016-16aa049b6a8d?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2123",
+    Africa:
+      "https://images.unsplash.com/photo-1547471080-7cc2caa01a7e?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471",
+    Europe:
+      "https://images.unsplash.com/photo-1499856871958-5b9627545d1a?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1420",
+    Oceania:
+      "https://images.unsplash.com/photo-1589330273594-fade1ee91647?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470",
+    "North America":
+      "https://images.unsplash.com/photo-1625230793635-9b429b1ff90f?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1467",
+    "South America":
+      "https://images.unsplash.com/photo-1543385426-191664295b58?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1595",
+  };
+
   function fetchCountryData() {
     axios
       .get("https://restcountries.com/v3.1/all")
@@ -40,7 +56,8 @@ const Search = () => {
             loadedRegion.push(resData[key].continents[0]);
           }
         }
-        console.log(loadedRegion);
+        const keysArr = Object.keys(regionArr);
+        console.log(keysArr, "どれでっか", loadedRegion);
         setRegionData(loadedRegion);
       })
       .catch((error) => console.log(error.message));
@@ -108,8 +125,9 @@ const Search = () => {
                     <figure>
                       <img
                         className="object-cover w-full"
-                        src="https://api.lorem.space/image/shoes?w=400&h=225"
-                        // src={`${country.flagImg}`}
+                        // original
+                        // src="https://api.lorem.space/image/shoes?w=400&h=225"
+                        src={regionArr[region]}
                         alt="Shoes"
                       />
                     </figure>
