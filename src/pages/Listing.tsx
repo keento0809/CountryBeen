6d032b85911b412cdd3afbba25b1050a7a43a99a
KEENTO <handball0321@gmail.com>
2022-05-28 14:50:16 -0700

    Refactor listing

---
+++ src/pages/Listing.tsx
@@ -0,0 +1,51 @@
+import DisplayWrapper from "../components/UI/Wrapper/DisplayWrapper";
+import { useSelector } from "react-redux";
+import { RootState } from "../store";
+import { CountryViewObj } from "../models/model";
+import CountryCard from "../components/UI/Card/CountryCard";
+import { useEffect, useState } from "react";
+
+interface ListingName {
+  name: string;
+}
+
+const Listing = ({ name }: ListingName) => {
+  // declare selector
+  const favoriteList = useSelector(
+    (state: RootState) => state.favoriteReducer.favoriteList
+  );
+  const beenToList = useSelector(
+    (state: RootState) => state.beenReducer.beenToList
+  );
+
+  function handleToggleBeenTo() {
+    console.log("I need to change something.");
+  }
+
+  return (
+    <DisplayWrapper>
+      <div className="">
+        <div className="title text-center text-white">
+          <h2 className="py-6 font-bold text-2xl">{name}</h2>
+        </div>
+        <div className="overflow-scroll max-h-680 md:max-h-640 md:flex flex-wrap">
+          {(name === "Record" ? beenToList : favoriteList).map(
+            (country, index) => {
+              return (
+                <CountryCard
+                  key={index}
+                  flagImg={`${country.flagImg}`}
+                  countryName={country.name}
+                  cca3={country.cca3}
+                  handleToggleBeenTo={handleToggleBeenTo}
+                />
+              );
+            }
+          )}
+        </div>
+      </div>
+    </DisplayWrapper>
+  );
+};
+
+export default Listing;
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,24 +3,21 @@ import CountryDetail from "./CountryDetail";
 import Hero from "./Hero";
 import Home from "./Home";
 import Search from "./Search";
-import BucketList from "./BucketList";
 import NotFound from "./NotFound";
-import BeenTo from "./BeenTo";
 import Region from "./Region";
-import Map from "./Map";
+import Listing from "./Listing";
 
 const Main = () => {
   return (
     <Routes>
       <Route path="/" element={<Hero />} />
       <Route path="/home" element={<Home />} />
-      <Route path="/record" element={<BeenTo />} />
-      <Route path="/bucket-list" element={<BucketList />} />
+      <Route path="/record" element={<Listing name={"Record"} />} />
+      <Route path="/bucket-list" element={<Listing name={"Bucket List"} />} />
       <Route path="/countries" element={<Search />} />
       <Route path="/countries/region/:region" element={<Region />} />
       <Route path="/countries/:id" element={<CountryDetail />} />
       {/* temporary */}
-      <Route path="/map" element={<Map />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
