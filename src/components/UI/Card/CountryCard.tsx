2144342b2c98b3aaca9da487415af6ebc6e95452
KEENTO <handball0321@gmail.com>
2022-05-28 12:52:39 -0700

    Add max-height in CountryDetail.tsx

--- src/components/UI/Card/CountryCard.tsx
+++ src/components/UI/Card/CountryCard.tsx
@@ -23,11 +23,9 @@ const CountryCard = ({
             />
           </figure>
           <div className="card-body transition-all overflow-hidden">
-            {/* <Link to={`/countries/${cca3}`} className="cursor-pointer"> */}
             <h2 className="font-extrabold text-3xl drop-shadow-md">
               {countryName}
             </h2>
-            {/* </Link> */}
             <div className="flex flex-row items-center">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -43,40 +41,8 @@ const CountryCard = ({
                   d="M9 5l7 7-7 7"
                 />
               </svg>
-              <span className="text-lg font-bold text-white">See Detail</span>
+              <span className="text-lg font-bold text-white">Detail</span>
             </div>
-            {/* <div className="icons flex flex-row" id={cca3}>
-              <svg
-                // onClick={handleToggleFavorite}
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-8 w-8 mr-4 cursor-pointer"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor" // #ff8ce4
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-                />
-              </svg>
-              <svg
-                onClick={handleToggleBeenTo}
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-8 w-8 cursor-pointer"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                strokeWidth={2}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-            </div> */}
           </div>
         </div>
       </Link>
