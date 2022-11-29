3a3aac988749ee27376e8d98552779f9b6130219
KEENTO <handball0321@gmail.com>
2022-11-29 10:49:49 -0800

    Cleaned up codes

--- src/features/countryService.ts
+++ src/features/countryService.ts
@@ -2,6 +2,7 @@ const countryService = {
   get: async () => {
     const res = await fetch("https://restcountries.com/v3.1/all");
     const data = await res.json();
+    console.log(data);
     return data;
   },
 };
