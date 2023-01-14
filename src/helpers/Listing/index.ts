48d63fc78d8d79d18e083d8bd5e6a07777f786b3
KEENTO <handball0321@gmail.com>
2023-01-14 09:27:31 -0800

    Created Listing helper and refactored codes

---
+++ src/helpers/Listing/index.ts
@@ -0,0 +1,40 @@
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../services/firebase";
+import { CountryViewObj } from "../../types/country";
+
+const currUserId = localStorage.getItem("currUser")
+  ? localStorage.getItem("currUser")
+  : "";
+
+function pushCountryData(dataArray: any) {
+  const listArr: CountryViewObj[] = [];
+  dataArray.forEach((resData: any) => {
+    listArr.push({
+      name: resData.name,
+      capital: resData.capital ? resData.capital : "",
+      population: resData.population,
+      continents: resData.continents,
+      currencies: resData.currencies ? resData.currencies : "",
+      languages: resData.languages,
+      coatOfArms: resData.coatOfArms.png,
+      flagImg: resData.flagImg,
+      flagIcon: resData.flag,
+      cca3: resData.cca3,
+      borders: resData.borders,
+    });
+  });
+  return listArr;
+}
+
+export async function fetchCurrentUserDataFromDB() {
+  const currUserRef = doc(db, "users", `${currUserId}`);
+  const querySnapshot = await getDoc(currUserRef);
+  const snapShotData = querySnapshot.data();
+  const favoriteListDataFromDb: CountryViewObj[] = pushCountryData(
+    snapShotData!.bucketList
+  );
+  const beenListDataFromDb: CountryViewObj[] = pushCountryData(
+    snapShotData!.record
+  );
+  return { favoriteListDataFromDb, beenListDataFromDb };
+}
