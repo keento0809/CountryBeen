dd6a17fa05a86f7fb7f5f09716a9f4c285318687
KEENTO <handball0321@gmail.com>
2023-02-12 12:15:46 -0800

    Upgraded the design of CountryCard

--- src/components/Card/CountryCard.tsx
+++ src/components/Card/CountryCard.tsx
@@ -1,20 +1,31 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { CountryCardProps } from "../../types/country";
 
 const CountryCard = ({ flagImg, countryName, cca3 }: CountryCardProps) => {
+  const [hover, setHover] = useState(false);
   return (
-    <div className="overflow-hidden rounded-2xl bg-transparent mr-auto mb-4">
-      <Link to={`/countries/${cca3}`} className="cursor-pointer">
-        <div className="card w-full max-w-374 md:max-w-340 lg:max-w-310 xl:max-w-320 md:basis-1/2 h-248 max-h-264 shadow-xl image-full hover:scale-105 transition-transform rounded-2xl bg-transparent">
+    <div className="overflow-hidden rounded-2xl mr-auto mb-4 transition-all">
+      <Link
+        to={`/countries/${cca3}`}
+        className="cursor-pointer"
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+      >
+        <div className="card w-full max-w-374 md:max-w-340 lg:max-w-310 xl:max-w-320 md:basis-1/2 h-248 max-h-264 shadow-xl image-full transition-transform rounded-2xl hover:scale-105">
           <figure>
             <img
-              className="object-cover w-full opacity-40 z-10"
+              className="object-cover w-ful z-10"
               src={flagImg}
               alt="Shoes"
             />
           </figure>
-          <div className="card-body rounded-2xl transition-all overflow-hidden">
-            <h2 className="font-extrabold text-3xl drop-shadow-md dark:text-slate-50">
+          <div
+            className={`${
+              hover ? "block" : "hidden"
+            } card-body rounded-2xl transition-all overflow-hidden bg-black opacity-40`}
+          >
+            <h2 className="font-extrabold text-3xl drop-shadow-xl dark:text-slate-50 opacity-1">
               {countryName}
             </h2>
             <div className="flex flex-row items-center">
