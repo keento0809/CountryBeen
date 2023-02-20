126b8a3071c6bb775cc253bfcc6c9c51e3747f86
KEENTO <handball0321@gmail.com>
2023-02-19 18:01:47 -0800

    Cleaned up codes in files in pages directory

---
+++ src/features/Search/SearchContainer.tsx
@@ -0,0 +1,37 @@
+import { Link } from "react-router-dom";
+import { regionArrFixed } from "../../data/data";
+import RegionCard from "../../components/Card/RegionCard";
+
+const SearchContainer = () => {
+  const regionData = regionArrFixed;
+  return (
+    <>
+      <div className="title text-center text-white">
+        <h2 className="py-6 font-bold text-2xl dark:text-gray-200">
+          Select Region
+        </h2>
+      </div>
+      <section className="countries lg:pb-6">
+        <div className="countries-container max-h-680 overflow-scroll lg:flex lg:flex-wrap lg:justify-between xl:justify-evenly">
+          {regionData.length === 0 && (
+            <p className="text-slate-800">Loading...</p>
+          )}
+          {regionData &&
+            regionData.map((region, index) => {
+              return (
+                <Link
+                  to={`/countries/region/${region}`}
+                  key={index}
+                  className="lg:block lg:max-w-374"
+                >
+                  <RegionCard region={region} />
+                </Link>
+              );
+            })}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default SearchContainer;
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,38 +1,10 @@
 import Wrapper from "../components/Wrapper/Wrapper";
-import { Link } from "react-router-dom";
-import { regionArrFixed } from "../data/data";
-import RegionCard from "../components/Card/RegionCard";
+import SearchContainer from "../features/Search/SearchContainer";
 
 const Search = () => {
-  const regionData = regionArrFixed;
   return (
     <Wrapper customStyle={`md:max-w-374 lg:max-w-780 xl:max-w-full`}>
-      <>
-        <div className="title text-center text-white">
-          <h2 className="py-6 font-bold text-2xl dark:text-gray-200">
-            Select Region
-          </h2>
-        </div>
-        <section className="countries lg:pb-6">
-          <div className="countries-container max-h-680 overflow-scroll lg:flex lg:flex-wrap lg:justify-between xl:justify-evenly">
-            {regionData.length === 0 && (
-              <p className="text-slate-800">Loading...</p>
-            )}
-            {regionData &&
-              regionData.map((region, index) => {
-                return (
-                  <Link
-                    to={`/countries/region/${region}`}
-                    key={index}
-                    className="lg:block lg:max-w-374"
-                  >
-                    <RegionCard region={region} />
-                  </Link>
-                );
-              })}
-          </div>
-        </section>
-      </>
+      <SearchContainer />
     </Wrapper>
   );
 };
