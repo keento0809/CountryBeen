43fb6ba2b24b127dd54847476cf07afa62a3c0f8
KEENTO <handball0321@gmail.com>
2023-03-21 15:17:23 -0700

    Created RegionWrapper component to fix the layout of elements in Search Page

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,11 +1,12 @@
 import Wrapper from "../components/Wrappers/Wrapper";
 import SearchContainer from "../features/Search/SearchContainer";
+import RegionWrapper from "../components/Wrappers/RegionWrapper";
 
 const Search = () => {
   return (
-    <Wrapper customStyle={`md:max-w-374 lg:max-w-780 xl:max-w-full`}>
+    <RegionWrapper customStyle={`md:max-w-374 lg:max-w-780 xl:max-w-full`}>
       <SearchContainer />
-    </Wrapper>
+    </RegionWrapper>
   );
 };
 
